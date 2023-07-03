4007e57bf63df3905fc03ff0775e5a19e6cf3a77
Mateo Rodriguez <luismateor73@gmail.com>
2023-07-02 20:03:37 -0500

    DELETE por PUT para actualizar las favorite movies

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -7,9 +7,9 @@ import serverAuth from "@/libs/serverAuth";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
 
-    if(req.method !== 'POST' && req.method !== 'DELETE') return res.status(405).end();
+    if(req.method !== 'POST' && req.method !== 'PUT') return res.status(405).end();
 
-    const { currentUser } = await serverAuth(req);
+    const { currentUser } = await serverAuth(req, res);
     const { movieId } = req.body;
 
     const existingMovie = await prismadb.movie.findUnique({
@@ -38,8 +38,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(200).json(user);
     }
 
-    if (req.method === 'DELETE') {
-
+    if (req.method === 'PUT') {      
       const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
 
       const updatedUser = await prismadb.user.update({
