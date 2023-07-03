90196c9ecf1a78b74e0fe6a1ac77f64752f19627
Mateo Rodriguez <luismateor73@gmail.com>
2023-07-02 20:00:48 -0500

    Boton para añadir o quitar de Favoritos

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { PlayIcon } from '@heroicons/react/24/solid';
 
 import { MovieInterface } from '@/types';
+import FavoriteButton from '@/components/FavoriteButton';
 
 interface MovieCardProps {
   data: MovieInterface;
@@ -70,6 +71,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             <div onClick={redirectToWatch} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
               <PlayIcon className="text-black w-4 lg:w-6" />
             </div>
+            <FavoriteButton movieId={data.id} />
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white">2023</span>
