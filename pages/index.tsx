79d5b9aafcd86d0955596707af602d9034ea043f
Mateo Rodriguez <luismateor73@gmail.com>
2023-07-02 20:01:43 -0500

    Mostrar la lista de movies, billboard y navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,41 @@
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react"
+import useMovieList from "@/hooks/useMoviesList";
+
+import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useFavorites from "@/hooks/useFavorites";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites} = useFavorites();
+
   return (
     <>
-      <h1>Netlix Clone</h1>
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList data={movies} title="Trending Now"/>
+        <MovieList data={favorites} title="Your Favorites"/>
+      </div>
     </>
   )
 }
